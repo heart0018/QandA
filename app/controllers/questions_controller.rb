b8867b29a557d8b3b3d9e44edadb9bc3aeb93481
heart0018~ <>
2025-06-08 03:39:06 +0900

    firstcommit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,35 @@
+class QuestionsController < ApplicationController
+  #質問の一覧を表示
+  def index
+
+  end
+
+  #質問の詳細を表示
+  def show
+
+  end
+
+  #質問の新規作成フォームを表示
+  def new
+
+  end
+
+  #質問の登録
+  def create
+
+  end
+
+  #質問の編集フォームを表示
+  def edit
+
+  end
+
+  #質問の更新
+  def update
+  end
+
+  #質問の削除
+  def destroy
+    
+  end
+end
